d0c95a60f69ccb8a21c8fba4aae7189a1d60788b
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-07 18:38:23 -0700

    started some basic javascript exercises

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let favouriteDrink = 'water';
+
+if (favouriteDrink === 'coffee') {
+  console.log(`I'll have coffee`);
+} else if (favouriteDrink === 'water') {
+  console.log(`I'll have water`);
+}
