b1df26430822fbda06ef4ca80771ebf497bc0fbd
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-07 23:18:21 -0700

    while loop exercises

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ while (currentSum <= 8000) {
   currentSum = currentSum + 1000;
 }
 console.log('Done');
+
+let bookPrice = 25;
+let budget = 80;
+while (budget >= bookPrice) {
+  budget -= bookPrice;
+  console.log(`I've bought a book!`);
+}
+console.log(`Rest: $${budget}`);
