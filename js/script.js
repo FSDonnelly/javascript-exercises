1f0e8f85c36e3c7a29b44e6118188a6b98bba19b
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-07 22:37:11 -0700

    for loop exercises

--- js/script.js
+++ js/script.js
@@ -23,9 +23,9 @@ c /= 2;
 console.log(a + b + c);
 
 //  For loop
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
-}
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
 
 for (let index = 40; index < 61; index++) {
   console.log(index);
@@ -38,4 +38,11 @@ for (let k = 10; k >= 2; k--) {
 let word = 'Hello';
 for (let a = 0; a < word.length; a++) {
   console.log(word[a]);
+}
+
+let num = prompt('enter a number', '');
+
+for (i = 1; i <= 10; i++) {
+  let res = num * i;
+  console.log(`${num} x ${i} = ${res}`);
 }
