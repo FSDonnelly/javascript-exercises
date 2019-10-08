fc0ac1c94b7161f2e644c8a7daa65d5a716a0621
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-07 23:04:29 -0700

    while loop exercises

---
+++ js/ifElse.js
@@ -0,0 +1,23 @@
+// If else example
+let favouriteDrink = 'beer';
+
+if (favouriteDrink === 'coffee') {
+  console.log(`I'll have coffee`);
+} else if (favouriteDrink === 'water') {
+  console.log(`I'll have water`);
+} else {
+  console.log(`I'll have nothing`);
+}
+
+let a = 10;
+let b = 5;
+let c = 2;
+console.log(a + b + c);
+// a = a + 5;
+// b = b - 1;
+// c = c / 2;
+// console.log(a + b + c);
+a += 5;
+b--;
+c /= 2;
+console.log(a + b + c);
--- js/script.js
+++ js/script.js
@@ -1,58 +1,8 @@
-// If else example
-let favouriteDrink = 'beer';
+//While loop
 
-if (favouriteDrink === 'coffee') {
-  console.log(`I'll have coffee`);
-} else if (favouriteDrink === 'water') {
-  console.log(`I'll have water`);
-} else {
-  console.log(`I'll have nothing`);
+let currentSum = 100;
+while (currentSum <= 8000) {
+  console.log(`I have $${currentSum}`);
+  currentSum = currentSum + 1000;
 }
-
-let a = 10;
-let b = 5;
-let c = 2;
-console.log(a + b + c);
-// a = a + 5;
-// b = b - 1;
-// c = c / 2;
-// console.log(a + b + c);
-a += 5;
-b--;
-c /= 2;
-console.log(a + b + c);
-
-//  For loop
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
-
-for (let index = 40; index < 61; index++) {
-  console.log(index);
-}
-
-for (let k = 10; k >= 2; k--) {
-  console.log(k);
-}
-
-let word = 'Hello';
-for (let a = 0; a < word.length; a++) {
-  console.log(word[a]);
-}
-
-let num = prompt('enter a number', '');
-
-for (i = 1; i <= 10; i++) {
-  let res = num * i;
-  console.log(`${num} x ${i} = ${res}`);
-}
-
-let number1 = +prompt('enter first number', '');
-let number2 = +prompt('enter second number', '');
-let sum = 0;
-for (let k = number1; k <= number2; k++) {
-  if (k % 2 === 0) {
-    sum += k;
-  }
-}
-console.log(sum);
+console.log('Done');
