a6975073af3261e5cd9afcdf44bda4482936d4fc
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-08 22:31:13 -0700

    callback, setTimeout, setInterval js exercises

--- js/return.js
+++ js/return.js
@@ -62,3 +62,21 @@ if (isPrime(num)) {
   console.log(`${num} is a composite number`);
   alert(`${num} is a composite number`);
 }
+
+//To be or not to be
+//toBeOrNotToBe
+function toCamelCase(str) {
+  let words = str.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] =
+      words[i][0].toUpperCase() + words[i].substring(1, words[i].length);
+  }
+  words[0] = words[0][0].toLowerCase() + words[0].substring(1, words[0].length);
+  let result = words.join('');
+  return result;
+}
+
+let str = prompt('Enter a sentence', '');
+let str2 = toCamelCase(str);
+
+console.log(str2);
--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 // Sandbox
-
-//To be or not to be
-//toBeOrNotToBe
-function toCamelCase(str) {
-  let words = str.split(' ');
-  for (let i = 0; i < words.length; i++) {
-    words[i] =
-      words[i][0].toUpperCase() + words[i].substring(1, words[i].length);
-  }
-  words[0] = words[0][0].toLowerCase() + words[0].substring(1, words[0].length);
-  let result = words.join('');
-  return result;
+function helloFunc() {
+  console.log('Hello');
 }
 
-let str = prompt('Enter a sentence', '');
-let str2 = toCamelCase(str);
+function byeFunc() {
+  console.log('Good bye!');
+}
 
-console.log(str2);
+function greeting(func) {
+  //body...
+  func();
+}
+
+greeting(helloFunc);
