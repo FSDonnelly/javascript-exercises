7ddc65c16e643a36a8fff399661552b8858a0181
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-09 15:48:57 -0700

    scope javascript exercises

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@
 // Scope
 let x = 2;
 
+// function printX() {
+//   // let x = 10;
+//   x = 10; // console prints 15
+//   console.log(x);
+// }
+
 function printX() {
   let x = 10;
+  for (let x = 0; x <= 2; x++) {
+    console.log(x);
+  }
   console.log(x);
 }
+
+printX();
 x = x + 5;
 console.log(x);
