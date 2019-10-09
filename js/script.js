91df60e62a900cb11ea1979821017b1f3c825f71
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-08 20:02:07 -0700

    functions with arguments using return exercises

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // Sandbox
+
+//To be or not to be
+//toBeOrNotToBe
+function toCamelCase(str) {
+  let words = str.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] =
+      words[i][0].toUpperCase() + words[i].substring(1, words[i].length);
+  }
+  words[0] = words[0][0].toLowerCase() + words[0].substring(1, words[0].length);
+
+  console.log(words);
+}
+
+let str = 'To be or not to be';
+toCamelCase(str);
