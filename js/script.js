ad8313eeb573bc059c8233ba745bb8239001b0cf
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-08 19:08:05 -0700

    functions with arguments exercises

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ function area(length, width) {
 
 area(20, 10);
 area(30, 20);
+
+let plane = 1100;
+let hotel = 800;
+let food = 600;
+
+function budget(plane, hotel, food) {
+  let total = plane + hotel + food;
+  let perDay = total / 14;
+  console.log(`Your vacation will cost you ${total}.`);
+  console.log(`Your daily cost is ${perDay}.`);
+}
+
+budget(plane, hotel, food);
+budget(1000, 700, 550);
+budget(1000, 1200, 700);
