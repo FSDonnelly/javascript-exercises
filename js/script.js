df6385be7ba1184296f10ca5745c992708ad2e24
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-09 22:14:37 -0700

    arrays javascript exercises

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // Sandbox
+//  Array iteration
+let nums = [1, 2, 3, 4];
+let sum = 0;
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i];
+}
+console.log(sum);
+
+let nums2 = [1, 2, 3, 4];
+let sum2 = 0;
+nums2.forEach(function(el, index, array) {
+  sum2 += el;
+});
+console.log(sum2);
