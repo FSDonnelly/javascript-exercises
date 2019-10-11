569f25fbd94b8fd2c748034c4c3e984fb84eedfa
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-10 19:04:04 -0700

    array javascript exercises

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@ nums2.forEach(function(el, index, array) {
   sum2 += el;
 });
 console.log(sum2);
+
+let array = [11, 9, 8, 6, 0, 0, 3];
+
+let even = 0;
+let odd = 0;
+let zeros = 0;
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  if (element === 0) {
+    zeros++;
+  } else if (element % 2 === 0) {
+    even++;
+  } else {
+    odd++;
+  }
+}
+console.log(`Evens: ${even}`);
+console.log(`Odds: ${odd}`);
+console.log(`Zeros: ${zeros}`);
