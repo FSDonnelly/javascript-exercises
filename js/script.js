31899e2692092128a885054c25041b11c4b154ad
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-10 20:12:30 -0700

    array javascript exercises

--- js/script.js
+++ js/script.js
@@ -1,35 +1,22 @@
 // Sandbox
 //  Array iteration
-let nums = [1, 2, 3, 4];
-let sum = 0;
-for (let i = 0; i < nums.length; i++) {
-  sum += nums[i];
-}
-console.log(sum);
 
-let nums2 = [1, 2, 3, 4];
-let sum2 = 0;
-nums2.forEach(function(el, index, array) {
-  sum2 += el;
-});
-console.log(sum2);
+let array = [100, -2, 16, 41, 21, 0, 6, -4];
 
-let array = [11, 9, 8, 6, 0, 0, 3];
+let minVal = array[0];
+let maxVal = array[0];
+let minIndex = 0;
+let maxIndex = 0;
 
-let even = 0;
-let odd = 0;
-let zeros = 0;
-
-for (let i = 0; i < array.length; i++) {
+for (let i = 1; i < array.length; i++) {
   const element = array[i];
-  if (element === 0) {
-    zeros++;
-  } else if (element % 2 === 0) {
-    even++;
-  } else {
-    odd++;
+  if (element < minVal) {
+    minVal = element;
+    minIndex = i;
+  } else if (element > maxVal) {
+    maxVal = element;
+    maxIndex = i;
   }
 }
-console.log(`Evens: ${even}`);
-console.log(`Odds: ${odd}`);
-console.log(`Zeros: ${zeros}`);
+console.log(`Min: ${minVal}; Index: ${minIndex}`);
+console.log(`Max: ${maxVal}; Index: ${maxIndex}`);
