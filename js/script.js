2fc7c1c48dd412224b7bd002f9d29e452f2106e6
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-12 18:51:58 -0700

    object javascript exercises

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ person['lastname'] = 'Smith';
 
 console.log(person['age']);
 console.log(person['lastname']);
+
+if (person.age >= 18) {
+  console.log('Person is an adult!');
+} else {
+  console.log('Person is not an adult!');
+}
+
+person.age = 12;
+
+if (person.age >= 18) {
+  console.log('Person is an adult!');
+} else {
+  console.log('Person is not an adult!');
+}
