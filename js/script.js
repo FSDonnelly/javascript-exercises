162adaef2a1eadbd2b59b313053c58496fe129df
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-14 22:47:43 -0700

    DOM and JS Selectors javascript exercises

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 
 console.dir(document);
 console.dir(window);
+
+// getElementByName()
+{
+  /* <h1 class="red-text">Greeting</h1>
+    <p class="red-text" id="main">Hello!</p>
+    <p class="green-text" name="wish">Have a nice day!</p> */
+}
+let name = document.getElementsByName('wish');
+console.dir(name);
+
+// getElementsByTagName()
+let tag = document.getElementsByTagName('p');
+console.dir(tag);
+
+// getElementsByClassName()
+let className = document.getElementsByClassName('red-text');
+console.dir(className);
