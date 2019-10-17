180d72c108a86d32659f05e7e1c58993317dcb60
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-16 21:26:43 -0700

    keyboard events javascript exercises

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 // Sandbox
 
 //Keyboard Events
-let inp = document.querySelector('input');
+let inp = document.querySelectorAll('input');
+console.log(inp);
+// inp.addEventListener('keyup', () => {
+//   console.log('keyup: ' + inp.value);
+// });
 
-inp.addEventListener('keyup', () => {
-  console.log('keyup: ' + inp.value);
-});
+// inp.addEventListener('keydown', () => {
+//   console.log('keydown: ' + inp.value);
+// });
 
-inp.addEventListener('keydown', () => {
-  console.log('keydown: ' + inp.value);
-});
-
-inp.addEventListener('keypress', () => {
-  console.log('keypress: ' + inp.value);
-});
+// inp.addEventListener('keypress', () => {
+//   console.log('keypress: ' + inp.value);
+// });
+for (let i = 0; i < inp.length; i++) {
+  const element = inp[i];
+  element.addEventListener('keyup', () => {
+    console.log('keyup: ' + element.value);
+  });
+}
